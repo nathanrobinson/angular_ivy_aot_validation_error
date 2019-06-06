ff736b4cf158542a28f1384fdd80f3c9b72290d2
Nathan Robinson <nrobinson@firebend.com>
2019-06-06 16:21:44 -0500

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { FormInputComponent } from "./form-input/form-input.component";
 
 @NgModule({
   declarations: [AppComponent, FormInputComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/form-input/form-input.component.ts
+++ src/app/form-input/form-input.component.ts
@@ -1,34 +1,19 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  AfterContentInit,
-  Output,
-} from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: "app-form-input",
   templateUrl: "./form-input.component.html",
   styleUrls: ["./form-input.component.css"],
 })
-export class FormInputComponent implements AfterContentInit {
+export class FormInputComponent {
   @Input() value: string;
   @Input() name: string;
   @Input() placeholder: string;
 
   @Output() valueChange = new EventEmitter<string>();
 
-  required = null;
-  maxLength = null;
-  minLength = null;
-  pattern = null;
-
-  ngAfterContentInit(): void {
-    setTimeout(() => {
-      this.required = "";
-      this.maxLength = 15;
-      this.minLength = 5;
-      this.pattern = /^[\w\d]*$/;
-    }, 10);
-  }
+  required = "";
+  maxLength = 15;
+  minLength = 5;
+  pattern = /^[\w\d]*$/;
 }
